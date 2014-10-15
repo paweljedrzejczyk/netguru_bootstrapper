25e7b79f35520e9079bf0c24f125e61545a1e0bc
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-10-15 20:47:58 +0200

    add structure parser

---
+++ lib/netguru_bootstrapper/structure_parser.rb
@@ -0,0 +1,10 @@
+require 'yaml'
+
+module NetguruBootstrapper
+  class StructureParser < OpenStruct
+    def initialize
+      path = File.expand_path('../../structure.yml', __FILE__)
+      super(YAML.load_file(path))
+    end
+  end
+end
